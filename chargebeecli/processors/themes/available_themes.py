1430d738ed6fd52ede5cf703b7c76d5855043260
Nandkishor Bhasker <bhasker.nandkishor@gmail.com>
2021-01-27 09:39:02 +0530

    making project public

---
+++ chargebeecli/processors/themes/available_themes.py
@@ -0,0 +1,17 @@
+available_themes = [
+    {
+        'name': 'basic',
+        'header-color': 'white',
+        'line-after-header-color': 'white',
+        'data-color': 'green'
+
+    }
+]
+
+available_theme = {
+    'name': 'basic',
+    'header-color': 'white',
+    'line-after-header-color': 'white',
+    'data-color': 'green'
+
+}
